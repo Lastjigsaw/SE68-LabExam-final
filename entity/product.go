5c51be6ac4c7464c3b812f3cbf2972bdcfb903dc
Lastjigsaw <khanathip_kpb@hotmail.com>
2026-01-06 20:19:32 +0700

    Create: entity product - close#2

---
+++ entity/product.go
@@ -0,0 +1,19 @@
+package entity 
+
+import (
+	"error"
+	"gorm.io/gorm"
+	"github.com/asaskevich/govalidator"
+)  
+
+func Product struct {
+	gorm.Model
+
+	Name 		string `json:"name"`
+	Price 	float64 `json:"price"`
+	Stock 	int			`json:"stokc"`
+	Description		string `json:"description"`
+
+}
+
+
